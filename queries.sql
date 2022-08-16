3cbe2505452c62a7bc6e18391b47667bb8c733d2
unknown <stephenjackson777@gmail.com>
2022-08-16 15:53:05 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
 --This script performs the following queries:
 
---DONEList the following details of each employee: employee number, last name, first name, sex, and salary.
---DONEList first name, last name, and hire date for employees who were hired in 1986.
---DONEList the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
---DONEList the department of each employee with the following information: employee number, last name, first name, and department name.
---DONEList first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
---DONEList all employees in the Sales department, including their employee number, last name, first name, and department name.
---DONEList all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
---DONEIn descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+--List first name, last name, and hire date for employees who were hired in 1986.
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
 --show available data in each table, limited to first 10 entires
 SELECT * FROM titles LIMIT (10);
